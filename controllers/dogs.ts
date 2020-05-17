29382cffa70a9e6474c2583ec2ee4e54113dae94
Saswata Mukherjee <saswataminsta@yahoo.com>
2020-05-17 15:12:06 +0530

    Added fetch methods

--- controllers/dogs.ts
+++ controllers/dogs.ts
@@ -35,3 +35,37 @@ export const createDog: HandlerFunc = async (c: Context) => {
     return c.json(error, 500);
   }
 };
+
+export const fetchAllDogs: HandlerFunc = async (c: Context) => {
+  try {
+    const fetchedDogs: Dog[] = await dogs.find();
+
+    if (fetchedDogs) {
+      const fetchedDogsList = fetchedDogs.length
+        ? fetchedDogs.map((dog) => {
+          const { _id: { $oid }, name, breed, age } = dog;
+          return { id: $oid, name, breed, age };
+        })
+        : [];
+      return c.json(fetchedDogsList, 200);
+    }
+  } catch (error) {
+    return c.json(error, 500);
+  }
+};
+
+export const fetchOneDog: HandlerFunc = async (c: Context) => {
+  try {
+    const { id } = c.params as { id: string };
+
+    const fetchedDog = await dogs.findOne({ _id: { "$oid": id } });
+
+    if (fetchedDog) {
+      const { _id: { $oid }, name, breed, age } = fetchedDog;
+      return c.json({ id: $oid, name, breed, age }, 200);
+    }
+    return c.string("Dog not found", 404);
+  } catch (error) {
+    return c.json(error, 500);
+  }
+};
