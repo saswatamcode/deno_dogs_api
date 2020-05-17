09a75e992b1f4d306a5518736ce2d7e275d1e7b8
Saswata Mukherjee <saswataminsta@yahoo.com>
2020-05-17 15:23:50 +0530

    Controllers added

--- controllers/dogs.ts
+++ controllers/dogs.ts
@@ -4,9 +4,11 @@ import {
 } from "https://deno.land/x/abc@v1.0.0-rc2/mod.ts";
 import db from "../config/db.ts";
 
+// DB collection made
 const database = db.getDatabase;
 const dogs = database.collection("dogs");
 
+// Dog type defined
 interface Dog {
   _id: {
     $oid: string;
@@ -64,6 +66,60 @@ export const fetchOneDog: HandlerFunc = async (c: Context) => {
       const { _id: { $oid }, name, breed, age } = fetchedDog;
       return c.json({ id: $oid, name, breed, age }, 200);
     }
+    return c.string("Dog not found", 404);
+  } catch (error) {
+    return c.json(error, 500);
+  }
+};
+
+export const updateDog: HandlerFunc = async (c: Context) => {
+  try {
+    const { id } = c.params as { id: string };
+
+    const body = await (c.body()) as {
+      name?: string;
+      breed?: string;
+      age?: string;
+    };
+
+    if (!Object.keys(body).length) {
+      return c.string("Request can't be empty", 400);
+    }
+
+    const fetchedDog = await dogs.findOne({ _id: { "$oid": id } });
+
+    if (fetchedDog) {
+      const { matchedCount } = await dogs.updateOne(
+        { _id: { "$oid": id } },
+        { $set: body },
+      );
+      if (matchedCount) {
+        return c.string("Dog updated successfully!", 204);
+      }
+      return c.string("Unable to update dog");
+    }
+
+    return c.string("Dog not found", 404);
+  } catch (error) {
+    return c.json(error, 500);
+  }
+};
+
+export const deleteDog: HandlerFunc = async (c: Context) => {
+  try {
+    const { id } = c.params as { id: string };
+
+    const fetchedDog = await dogs.findOne({ _id: { "$oid": id } });
+
+    if (fetchedDog) {
+      const deleteCount = await dogs.deleteOne({ _id: { "$oid": id } });
+
+      if (deleteCount) {
+        return c.string("Dog deleted successfully!", 204);
+      }
+      return c.string("Unable to delete dog");
+    }
+
     return c.string("Dog not found", 404);
   } catch (error) {
     return c.json(error, 500);
